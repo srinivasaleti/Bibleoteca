ce75ab29f22cc78fcabffd880153965769a6cce3
srinivasarao <srinivra@thoughtworks.com>
2017-08-17 11:02:08 +0530

    Create a representation for user

---
+++ Biblioteca/src/com/patashala57/model/User.java
@@ -0,0 +1,28 @@
+package com.patashala57.model;
+
+class User {
+
+    private final String name;
+    private final String libraryNo;
+    private final String password;
+    private final String email;
+    private final String phoneNumber;
+
+    public User(String name, String libraryNo, String password, String email, String phoneNumber) {
+        this.name = name;
+        this.libraryNo = libraryNo;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    boolean hasSameCredentials(String libraryNo, String password) {
+        return this.libraryNo.equals(libraryNo) && this.password.equals(password);
+    }
+
+    String representation() {
+       return String.format("%-35s %-35s %-35s",name,email,phoneNumber);
+    }
+
+}
+
---
+++ Biblioteca/test/com/patashala57/model/UserTest.java
@@ -0,0 +1,33 @@
+package com.patashala57.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void beforeEach() {
+        this.user = new User("srinu", "123-123424", "1234", "srinivas.aleti03@gmail.com", "9848981244");
+    }
+
+    @Test
+    void successfulLogin() {
+        assertTrue(user.hasSameCredentials("123-123424", "1234"));
+    }
+
+    @Test
+    void unSuccessfulLogin() {
+        assertFalse(user.hasSameCredentials("123-123414", "1434"));
+    }
+
+    @Test
+    void representationOfUser() {
+        String expected = String.format("%-35s %-35s %-35s", "srinu", "srinivas.aleti03@gmail.com", "9848981244");
+        assertEquals(this.user.representation(), expected);
+    }
+
+}
